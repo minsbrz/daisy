62cae02c82f4539f0cc53e030a9112eb4748e4c8
Kidong Lee <mykidong@gmail.com>
2012-03-14 10:09:58 +0900

    xx.

--- bundles/data-collection/data-collection-service-component/src/test/java/kafka/examples/KafkaProperties.java
+++ bundles/data-collection/data-collection-service-component/src/test/java/kafka/examples/KafkaProperties.java
@@ -18,7 +18,7 @@ package kafka.examples;
 
 public interface KafkaProperties
 {
-  final static String zkConnect = "127.0.0.1:2181";
+  final static String zkConnect = "localhost:2181";
   final static  String groupId = "group1";
   final static String topic = "topic1";
   final static String kafkaServerURL = "localhost";
